2e8a5168ee7fee96b61f2e7fcb5a0001bf719d4a
zzfalg <160737286+zzfalg@users.noreply.github.com>
2024-03-17 09:18:25 +0800

    Add files via upload

---
+++ 初学2.py
@@ -0,0 +1,18 @@
+print("hello world!!!")
+print("hello python")
+# 我是单行注释
+"""
+神奇的啊giao
+"""
+# 定义一个变量
+money = 50
+# 买了一个冰激淋，花了10元
+money = money - 10
+print("还剩：",money,"元")
+# 定义一个变量
+money = 50
+# 买了一个冰激凌和可乐，还剩多少
+冰激凌 = 10
+可乐 = 5
+money = money - 10 - 5
+print("还剩:",money)
