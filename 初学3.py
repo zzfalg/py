11296ba3812aef524fd492cb7c629d1df906c1c4
zzfalg <160737286+zzfalg@users.noreply.github.com>
2024-03-22 09:05:38 +0800

    Add files via upload

---
+++ 初学3.py
@@ -0,0 +1,12 @@
+name = "丁真"
+name_type = type(name)
+print(name_type)
+# 将数字类型转换成字符串
+num_str = str(666)
+print(type(num_str),num_str)
+
+float_str = str(12.666)
+print(type(float_str),float_str)
+# 将字符串转换为数字
+num = int("666")
+print(type(num),num)
